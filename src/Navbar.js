3e8b7ea76a38335ee1e78c1340c5e69dfeea9902
Harish0831 <harishvicky64@gmail.com>
2025-01-11 12:48:26 +0530

    Stop Watch

---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar ${scrolled ? "navbar-scrolled" : ""}`}>
+      <ul className="nav-links">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
